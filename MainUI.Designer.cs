96ccd1ad38e43d70db6a8c80756be8d369a885ef
Matt <mateuszgrabowski05@gmail.com>
2025-03-01 20:43:41 +0100

    Revert "fixed crash when a city is on the edge of an image"
    This reverts commit 98dadb46ac7ade7aa978342bda7696a2724dc84a.

--- MainUI.Designer.cs
+++ MainUI.Designer.cs
@@ -391,7 +391,7 @@
             Margin = new Padding(4, 3, 4, 3);
             MaximizeBox = false;
             Name = "MainUI";
-            Text = "Image to Scenario Converter v3.1.1";
+            Text = "Image to Scenario Converter";
             Load += MainUI_Load;
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)PosterizationTrackBar).EndInit();
